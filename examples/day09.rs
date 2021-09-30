31d0fcb61395da63647a0b87ad3608a43418899a
Nathan McIntosh <Nathan.McIntosh@jhuapl.edu>
2021-09-30 08:44:14 -0400

    finished day 9

--- examples/day09.rs
+++ examples/day09.rs
@@ -20,6 +20,26 @@ fn part1(ints: &[usize], preamble_size: usize) -> usize {
         .expect("Could not find a bad number")
 }
 
+fn part2(ints: &[usize], to_sum_to: usize) -> usize {
+    for window_size in 2..ints.len() {
+        let good_window = ints
+            .windows(window_size)
+            .find(|&w| w.iter().sum::<usize>() == to_sum_to);
+        if let Some(winow) = good_window {
+            let min = winow
+                .iter()
+                .reduce(|a, b| a.min(b))
+                .expect("Could not find a minimum");
+            let max = winow
+                .iter()
+                .reduce(|a, b| a.max(b))
+                .expect("Could not find a minimum");
+            return min + max;
+        }
+    }
+    panic!("Could not find a solution");
+}
+
 fn main() {
     println!("Hello world");
     let ints: Vec<usize> = std::fs::read_to_string("inputs/day09.txt")
@@ -34,7 +54,16 @@ fn main() {
         "Part 1 took {:.6} microseconds",
         part1_time.elapsed().as_micros()
     );
+
+    let part2_time = std::time::Instant::now();
+    let part2_soln = part2(&ints, part1_soln);
+    println!(
+        "Part 2 took {:.6} microseconds",
+        part2_time.elapsed().as_micros()
+    );
+
     println!("First Puzzle: {:?}", part1_soln);
+    println!("Second Puzzle: {:?}", part2_soln);
 }
 
 #[test]
@@ -45,5 +74,16 @@ fn test_part1() {
     let preamble_size: usize = 5;
     let got = part1(&ints, preamble_size);
     let expected: usize = 127;
+    assert_eq!(got, expected);
+}
+
+#[test]
+fn test_part2() {
+    let ints: Vec<usize> = vec![
+        35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576,
+    ];
+    let to_sum_to: usize = 127;
+    let got = part2(&ints, to_sum_to);
+    let expected: usize = 62;
     assert_eq!(got, expected);
 }
